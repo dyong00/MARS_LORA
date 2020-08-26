0d5ca7a32c39e4ad4bf753322527116d73247ec2
Shin hyeon joeng <51795683+dyong00@users.noreply.github.com>
2020-08-26 22:02:16 +0900

    Add files via upload

---
+++ LoRaPingPongTest1.ino
@@ -0,0 +1,108 @@
+#include <SoftwareSerial.h>
+#include "SNIPE.h"
+
+#define PING  1
+#define PONG  2
+
+#define CODE  PING    /* Please define PING or PONG */
+
+#define TXpin D9
+#define RXpin D8
+#define ATSerial Serial
+
+//16byte hex key
+String lora_app_key = "11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff 00";  
+
+SoftwareSerial DebugSerial(RXpin,TXpin);
+SNIPE SNIPE(ATSerial);
+
+void setup() {
+  ATSerial.begin(115200);
+
+  // put your setup code here, to run once:
+  while(ATSerial.read()>= 0) {}
+  while(!ATSerial);
+
+  DebugSerial.begin(115200);
+
+  /* SNIPE LoRa Initialization */
+  if (!SNIPE.lora_init()) {
+    DebugSerial.println("SNIPE LoRa Initialization Fail!");
+    while (1);
+  }
+
+  /* SNIPE LoRa Set Appkey */
+  if (!SNIPE.lora_setAppKey(lora_app_key)) {
+    DebugSerial.println("SNIPE LoRa app key value has not been changed");
+  }
+  
+  /* SNIPE LoRa Set Frequency */
+  if (!SNIPE.lora_setFreq(LORA_CH_1)) {
+    DebugSerial.println("SNIPE LoRa Frequency value has not been changed");
+  }
+
+  /* SNIPE LoRa Set Spreading Factor */
+  if (!SNIPE.lora_setSf(LORA_SF_7)) {
+    DebugSerial.println("SNIPE LoRa Sf value has not been changed");
+  }
+
+  /* SNIPE LoRa Set Rx Timeout 
+   * If you select LORA_SF_12, 
+   * RX Timout use a value greater than 5000  
+  */
+  if (!SNIPE.lora_setRxtout(5000)) {
+    DebugSerial.println("SNIPE LoRa Rx Timout value has not been changed");
+  }  
+    
+  DebugSerial.println("SNIPE LoRa PingPong Test");
+}
+
+void loop() {
+  
+#if CODE == PING //조끼
+        if (SNIPE.lora_send("mars ping"))
+        {
+          DebugSerial.println("send success");
+          
+          String ver = SNIPE.lora_recv();
+          DebugSerial.println(ver);
+
+          if (ver != "AT_RX_TIMEOUT" && ver != "AT_RX_ERROR")
+          {
+            DebugSerial.println("recv success");
+            DebugSerial.println(SNIPE.lora_getRssi());
+            DebugSerial.println(SNIPE.lora_getSnr());            
+          }
+          else
+          {
+            DebugSerial.println("recv fail");
+            delay(500);
+          }
+        }
+       delay(1000);
+       
+#elif CODE == PONG //드론
+        String ver = SNIPE.lora_recv();
+        delay(300);
+
+        DebugSerial.println(ver);
+        
+        if (ver != "AT_RX_TIMEOUT" && ver != "AT_RX_ERROR")
+        {
+          DebugSerial.println("recv success");
+          DebugSerial.println(SNIPE.lora_getRssi());
+          DebugSerial.println(SNIPE.lora_getSnr());
+
+          if(SNIPE.lora_send("lora pong"))
+          {
+            DebugSerial.println("send success");
+          }
+          else
+          {
+            DebugSerial.println("send fail");
+            delay(500);
+          }
+        }
+       delay(1000);
+#endif
+}
